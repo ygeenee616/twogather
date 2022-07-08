ca0b4056b344b2e38680c916bf3eda36cb55aef6
나해란 <haeran@nahaelan-ui-MacBookAir.local>
2022-07-08 15:38:46 +0900

    fix: router 태그 수정

--- client/src/App.js
+++ client/src/App.js
@@ -5,34 +5,39 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      {/* <Routes>
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/register/:userType" element={<RegisterForm />} />
-        <Route path="/myPage" element={<MyPage />} />
-        <Route path="/myPage/addReview" element={<AddReview />} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/register/:userType" element={<RegisterForm />} />
+          <Route path="/myPage" element={<MyPage />} />
+          <Route path="/myPage/addReview" element={<AddReview />} />
 
-        <Route path="/notice" element={<Notice />} />
-        <Route path="/addNotice" element={<AddNotice />} />
+          <Route path="/notice" element={<Notice />} />
+          <Route path="/addNotice" element={<AddNotice />} />
 
-        <Route path="/admin" element={<AdminHome />} />
-        <Route path="/admin/userList" element={<AdminUserList />} />
-        <Route path="/admin/bookList" element={<AdminBookList />} />
-        <Route path="/admin/bookList/bookDetail/:bookId" element={<AdminBookDetail />} />
+          <Route path="/admin" element={<AdminHome />} />
+          <Route path="/admin/userList" element={<AdminUserList />} />
+          <Route path="/admin/bookList" element={<AdminBookList />} />
+          <Route
+            path="/admin/bookList/bookDetail/:bookId"
+            element={<AdminBookDetail />}
+          />
 
-        <Route path="/host" element={<HostHome />} />
-        <Route path="/host/addHost" element={<AddHost />} />
-        <Route path="/host/addSpace" element={<AddSpace />} />
-        <Route path="/host/bookList" element={<HostBookList />} />
-        <Route path="/host/spaceList" element={<HostSpaceList />} />
-        <Route path="/host/updateSpace" element={<UpdateSpace />} />
+          <Route path="/host" element={<HostHome />} />
+          <Route path="/host/addHost" element={<AddHost />} />
+          <Route path="/host/addSpace" element={<AddSpace />} />
+          <Route path="/host/bookList" element={<HostBookList />} />
+          <Route path="/host/spaceList" element={<HostSpaceList />} />
+          <Route path="/host/updateSpace" element={<UpdateSpace />} />
 
-        <Route path="/list/:searchInput" element={<ProductList />} />
-        <Route path="/detail/:spaceId" element={<Detail />} />
-        <Route path="/book" element={<Book />} />
+          <Route path="/list/:searchInput" element={<ProductList />} />
+          <Route path="/detail/:spaceId" element={<Detail />} />
+          <Route path="/book" element={<Book />} />
 
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes> */}
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
