d86f87ad44e6db1dfc225df7631a93f8d1ff0340
[SW2/서울/2반] 김태훈 <kth5954@gmail.com>
2022-07-08 14:58:00 +0900

    setting: typeORM User Entity

--- server/src/users/entities/user.entity.ts
+++ server/src/users/entities/user.entity.ts
@@ -1,1 +1,16 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  userId: number;
+
+  @Column()
+  nickname: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  phoneNumber: string;
+}
