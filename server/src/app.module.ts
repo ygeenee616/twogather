01cf3f47e3248b35aa158b56ae74fb510ac4f2cd
[SW2/서울/2반] 김태훈 <kth5954@gmail.com>
2022-07-10 22:45:06 +0900

    setting: dotenv 파일 설정

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -10,7 +10,10 @@ import * as Joi from 'joi';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '.env',
+      envFilePath:
+        process.env.NODE_ENV === 'production'
+          ? '.production.env'
+          : '.development.env',
       validationSchema: Joi.object({
         DB_HOST: Joi.string().required(),
         DB_PORT: Joi.string().required(),
