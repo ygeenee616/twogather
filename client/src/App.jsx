e4929a66fe8832caa8070967c429fe99a9d90fda
나해란 <haeran@nahaelan-ui-MacBookAir.local>
2022-07-11 15:08:47 +0900

    chore: 파일경로 수정

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
