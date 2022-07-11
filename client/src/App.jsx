a506ca55cea2942391f8146626a0c29c587339bf
miji <mijiunknown@gmail.com>
2022-07-12 02:11:02 +0900

    feat: react-recux, redux-toolkit 도입

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,44 +1,51 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import { Provider } from "react-redux";
+import store from "./store/Store";
+
+import Detail from "./pages/Detail";
+import Book from "./pages/Book";
 
 function App() {
   return (
-    <div className="App">
-      {/*<BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/register/:userType" element={<RegisterForm />} />
-          <Route path="/myPage" element={<MyPage/>}/>
-          <Route path="/myPage/addReview" element={<AddReview />} />
+    <Provider store={store}>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            {/* <Route path="/login" element={<LoginForm />} />
+            <Route path="/register/:userType" element={<RegisterForm />} />
+            <Route path="/myPage" element={<MyPage />} />
+            <Route path="/myPage/addReview" element={<AddReview />} />
 
-          <Route path="/notice" element={<Notice />} />
-          <Route path="/addNotice" element={<AddNotice />} />
+            <Route path="/notice" element={<Notice />} />
+            <Route path="/addNotice" element={<AddNotice />} />
 
-          <Route path="/admin" element={<AdminHome />} />
-          <Route path="/admin/userList" element={<AdminUserList />} />
-          <Route path="/admin/bookList" element={<AdminBookList />} />
-          <Route
-            path="/admin/bookList/bookDetail/:bookId"
-            element={<AdminBookDetail />} />
+            <Route path="/admin" element={<AdminHome />} />
+            <Route path="/admin/userList" element={<AdminUserList />} />
+            <Route path="/admin/bookList" element={<AdminBookList />} />
+            <Route
+              path="/admin/bookList/bookDetail/:bookId"
+              element={<AdminBookDetail />}
+            />
 
-          <Route path="/host" element={<HostHome />} />
-          <Route path="/host/addHost" element={<AddHost />} />
-          <Route path="/host/addSpace" element={<AddSpace />} />
-          <Route path="/host/bookList" element={<HostBookList />} />
-          <Route path="/host/spaceList" element={<HostSpaceList />} />
-          <Route path="/host/updateSpace" element={<UpdateSpace />} />
+            <Route path="/host" element={<HostHome />} />
+            <Route path="/host/addHost" element={<AddHost />} />
+            <Route path="/host/addSpace" element={<AddSpace />} />
+            <Route path="/host/bookList" element={<HostBookList />} />
+            <Route path="/host/spaceList" element={<HostSpaceList />} />
+            <Route path="/host/updateSpace" element={<UpdateSpace />} />
 
-          <Route path="/list/:searchInput" element={<ProductList />} />
-          <Route path="/detail/:spaceId" element={<Detail />} />
-          <Route path="/book" element={<Book />} />
+            <Route path="/list/:searchInput" element={<ProductList />} />
+            <Route path="/detail/:spaceId" element={<Detail />} />
+            <Route path="/book" element={<Book />} />
 
-          <Route path="/" element={<Home />} />
-          <Route path="*" element={<NotFound />} /> 
-        </Routes>
-      </BrowserRouter>*/}
-    </div>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<NotFound />} /> */}
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </Provider>
   );
 }
 
---
+++ client/src/Store.jsx
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import bookSlice from "./slices/BookSlice";
+
+// slice 안에 있는 reducer를 모아 store를 생성
+const store = configureStore({
+  reducers: {
+    book: bookSlice.reducer,
+  },
+});
+
+export default store;
