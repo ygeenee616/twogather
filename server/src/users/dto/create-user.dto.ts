1b70d20fa1daee69472241db27eb3eeed8cd4c37
[SW2/서울/2반] 김태훈 <kth5954@gmail.com>
2022-07-13 18:31:30 +0900

    feat: 유저 API 문서 작성(swagger)

--- server/src/users/dto/create-user.dto.ts
+++ server/src/users/dto/create-user.dto.ts
@@ -6,14 +6,17 @@ import {
   MaxLength,
   MinLength,
 } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CreateUserDto {
   @IsEmail()
+  @ApiProperty({ description: '이메일' })
   readonly email: string;
 
   @IsString()
   @MinLength(4)
   @MaxLength(20)
+  @ApiProperty({ description: '비밀번호(4~20자)' })
   readonly password: string;
 
   @IsString()
@@ -22,25 +25,9 @@ export class CreateUserDto {
   @Matches(/^[a-zA-Z0-9]*$/, {
     message: '닉네임은 영어 대/소문자, 숫자만 가능합니다',
   })
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  @ApiProperty({ description: '닉네임(4~20자, 영문 대소문자, 숫자만 가능)' })
   readonly nickname: string;
-
-  @IsString()
-  @IsOptional()
-  readonly name: string;
-
-  @IsString()
-  @IsOptional()
-  readonly sex: string;
-
-  @IsString()
-  @IsOptional()
-  readonly profileImage: string;
-
-  @IsString()
-  @IsOptional()
-  readonly phoneNumber: string;
-
-  @IsString()
-  @IsOptional()
-  readonly businessNumber: string;
 }
--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -16,26 +16,58 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/users.entity';
 import { UsersService } from './users.service';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiCreatedResponse,
+} from '@nestjs/swagger';
 
 @Controller('api/users')
+@ApiTags('유저 API')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post('/sign-up')
+  @ApiOperation({
+    summary: '회원가입 API',
+    description: '유저를 생성한다.(이름, 성별, 프로필 사진, 휴대폰 번호, )',
+  })
+  @ApiCreatedResponse({
+    description: '유저 생성 완료(입력값 외 null값).',
+    type: Object,
+  })
   async signup(@Body(ValidationPipe) createUserDto: CreateUserDto) {
     return this.usersService.register(createUserDto);
   }
 
   @Post('/sign-in')
+  @ApiOperation({ summary: '로그인 API', description: '로그인한다.' })
+  @ApiCreatedResponse({
+    description: '로그인 성공, access token 발급.',
+  })
   async login(@Req() req, @Body(ValidationPipe) userData: AuthCredentialDto) {
     const accessToken = await this.usersService.login(userData);
-    return { accessToken: accessToken };
+    return {
+      statusCode: 200,
+      message: '로그인 성공',
+      accessToken: accessToken,
+    };
   }
   @Get()
+  @ApiOperation({
+    summary: '유저 목록 조회 API',
+    description: '전체 유저를 조회한다.',
+  })
+  @ApiResponse({ description: '전체 유저 조회', type: User })
   findAll() {
     try {
       const users: Promise<User[]> = this.usersService.findAll();
-      return users;
+      return {
+        statusCode: 200,
+        message: '유저 조회 성공',
+        data: users,
+      };
     } catch (error) {
       throw error;
     }
@@ -43,19 +75,38 @@ export class UsersController {
 
   // 내 정보조회
   @Get('/info')
+  @ApiOperation({
+    summary: '내 정보 조회 API',
+    description: '내 정보를 조회한다.',
+  })
+  @ApiCreatedResponse({ description: '내 정보 조회', type: User })
   @UseGuards(AuthGuard())
   findMyInfo(@Req() req) {
-    return req.user;
+    return {
+      statusCode: 200,
+      message: 'so 조회 성공',
+      data: req.users,
+    };
   }
 
   // admin 기능
   @Get('/:id')
+  @ApiOperation({
+    summary: '특정 유저 조회 API',
+    description: '특정 유저를 조회한다.',
+  })
+  @ApiCreatedResponse({ description: '특정 유저 조회', type: User })
   findOneById(@Param('id') id: number): Promise<User> {
     return this.usersService.findOne(id);
   }
 
   // admin 기능
   @Get('/email/:email')
+  @ApiOperation({
+    summary: 'email로 조회 API',
+    description: 'email로 특정 유저를 조회한다.',
+  })
+  @ApiCreatedResponse({ description: 'email로 조회', type: User })
   findOneByEmail(@Param('email') email: string): Promise<User> {
     return this.usersService.findOneByEmail(email);
   }
@@ -63,18 +114,35 @@ export class UsersController {
   //마이페이지 수정
   @Patch()
   @UseGuards(AuthGuard())
+  @ApiOperation({
+    summary: '내 정보 수정 API',
+    description: '내 정보를 수정한다.',
+  })
+  @ApiCreatedResponse({ description: '내 정보 수정', type: User })
   updateUserInfo(@Req() req, @Body() userData: UpdateUserDto) {
     const updatedUser = this.usersService.update(req.user.id, userData);
     return updatedUser;
   }
 
+  // admin 기능
   @Delete('/:id')
+  @ApiOperation({
+    summary: '특정 유저 삭제 API',
+    description: '특정 유저를 삭제한다.',
+  })
+  @ApiCreatedResponse({ description: '내 정보 조회', type: User })
   remote(@Param('id') id: number) {
     this.usersService.remove(id);
   }
 
+  // 회원 탈퇴
   @Delete()
   @UseGuards(AuthGuard())
+  @ApiOperation({
+    summary: '회원 탈퇴 API',
+    description: '회원 탈퇴를 진행한다.',
+  })
+  @ApiCreatedResponse({ description: '회원 탈퇴 성공', type: User })
   withdrawal(@Req() req) {
     return this.usersService.remove(req.user.id);
   }
