8cc14a283a08e4d7c04dfd34dd7dccb1b95c2352
[SW2/서울/2반] 김태훈 <kth5954@gmail.com>
2022-07-14 01:50:19 +0900

    feature: Swagger 관련 데코레이션 수정

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -22,6 +22,7 @@ import {
   ApiResponse,
   ApiCreatedResponse,
 } from '@nestjs/swagger';
+import { access } from 'fs';
 
 @Controller('api/users')
 @ApiTags('유저 API')
@@ -33,18 +34,26 @@ export class UsersController {
     summary: '회원가입 API',
     description: '유저를 생성한다.(이름, 성별, 프로필 사진, 휴대폰 번호, )',
   })
-  @ApiCreatedResponse({
+  @ApiResponse({
+    status: 201,
     description: '유저 생성 완료(입력값 외 null값).',
-    type: Object,
+    type: User,
   })
   async signup(@Body(ValidationPipe) createUserDto: CreateUserDto) {
-    return this.usersService.register(createUserDto);
+    const newUser: Promise<User> = this.usersService.register(createUserDto);
+    return {
+      status: 201,
+      description: '유저 생성 완료(입력값 외 null값).',
+      data: newUser,
+    };
   }
 
   @Post('/sign-in')
   @ApiOperation({ summary: '로그인 API', description: '로그인한다.' })
-  @ApiCreatedResponse({
+  @ApiResponse({
+    status: 200,
     description: '로그인 성공, access token 발급.',
+    type: String,
   })
   async login(@Req() req, @Body(ValidationPipe) userData: AuthCredentialDto) {
     const accessToken = await this.usersService.login(userData);
@@ -60,7 +69,11 @@ export class UsersController {
     summary: '유저 목록 조회 API',
     description: '전체 유저를 조회한다.',
   })
-  @ApiResponse({ description: '전체 유저 조회', type: User })
+  @ApiResponse({
+    status: 200,
+    description: '전체 유저 조회',
+    type: Array<User>,
+  })
   findAll() {
     try {
       const users: Promise<User[]> = this.usersService.findAll();
@@ -80,12 +93,12 @@ export class UsersController {
     summary: '내 정보 조회 API',
     description: '내 정보를 조회한다.',
   })
-  @ApiCreatedResponse({ description: '내 정보 조회', type: User })
+  @ApiResponse({ status: 200, description: '내 정보 조회', type: User })
   @UseGuards(AuthGuard())
   findMyInfo(@Req() req) {
     return {
       statusCode: 200,
-      message: 'so 조회 성공',
+      message: '내 정보 조회 성공',
       data: req.users,
     };
   }
@@ -96,7 +109,7 @@ export class UsersController {
     summary: '특정 유저 조회 API',
     description: '특정 유저를 조회한다.',
   })
-  @ApiCreatedResponse({ description: '특정 유저 조회', type: User })
+  @ApiResponse({ status: 200, description: '특정 유저 조회', type: User })
   findOneById(@Param('id') id: number): Promise<User> {
     return this.usersService.findOne(id);
   }
@@ -107,7 +120,7 @@ export class UsersController {
     summary: 'email로 조회 API',
     description: 'email로 특정 유저를 조회한다.',
   })
-  @ApiCreatedResponse({ description: 'email로 조회', type: User })
+  @ApiResponse({ status: 200, description: '이메일로 조회 성공', type: User })
   findOneByEmail(@Param('email') email: string): Promise<User> {
     return this.usersService.findOneByEmail(email);
   }
@@ -119,10 +132,10 @@ export class UsersController {
     summary: '내 정보 수정 API',
     description: '내 정보를 수정한다.',
   })
-  @ApiCreatedResponse({ description: '내 정보 수정', type: User })
-  updateUserInfo(@Req() req, @Body() userData: UpdateUserDto) {
+  @ApiResponse({ status: 201, description: '내 정보 수정 성공', type: User })
+  updateUserInfo(@Req() req, @Body(ValidationPipe) userData: UpdateUserDto) {
     const updatedUser = this.usersService.update(req.user.id, userData);
-    return updatedUser;
+    return { status: 201, description: '내 정보 수정 성공', data: updatedUser };
   }
 
   // admin 기능
@@ -131,9 +144,9 @@ export class UsersController {
     summary: '특정 유저 삭제 API',
     description: '특정 유저를 삭제한다.',
   })
-  @ApiCreatedResponse({ description: '내 정보 조회', type: User })
-  remote(@Param('id') id: number) {
-    this.usersService.remove(id);
+  @ApiResponse({ status: 201, description: '특정 유저 삭제 성공' })
+  removeUser(@Param('id') id: number) {
+    return this.usersService.remove(id);
   }
 
   // 회원 탈퇴
@@ -143,7 +156,11 @@ export class UsersController {
     summary: '회원 탈퇴 API',
     description: '회원 탈퇴를 진행한다.',
   })
-  @ApiCreatedResponse({ description: '회원 탈퇴 성공', type: User })
+  @ApiResponse({
+    status: 201,
+    description: '회원 탈퇴 성공',
+    type: User,
+  })
   withdrawal(@Req() req) {
     return this.usersService.remove(req.user.id);
   }
