72f05eb809c7d2195bb0e84d26004dd21c99cf05
나해란 <haeran@nahaelan-ui-MacBookAir.local>
2022-07-14 01:54:10 +0900

    fix: 잔에러 고침

--- client/src/pages/ProductList.jsx
+++ client/src/pages/ProductList.jsx
@@ -105,7 +105,7 @@ const ProductWrap = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   grid-template-rows: 1fr 1fr 1fr;
-  gap: 1vw;
+  gap: 2%;
 `;
 
 const SelectButton = styled.button`
