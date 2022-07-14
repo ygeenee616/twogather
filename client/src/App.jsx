a91ccb32c61228abf54efcd42801aaa4dfebb0e3
YeaJeong <bananana0118@gmail.com>
2022-07-15 00:53:56 +0900

    FE/feat/componentSeperation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -33,11 +33,11 @@ function App() {
       <div className="App">
         <BrowserRouter>
           <Header></Header>
-          {/* <Routes>
+          <Routes>
             <Route path="/login" element={<LoginForm />} />
             <Route path="/register/:userType" element={<RegisterForm />} />
             <Route path="/myPage" element={<MyPage />} />
-            <Route path="/myPage/addReview" element={<AddReview />} />
+            {/* <Route path="/myPage/addReview" element={<AddReview />} /> */}
 
             <Route path="/notice" element={<Notice />} />
             <Route path="/addNotice" element={<AddNotice />} />
@@ -51,19 +51,21 @@ function App() {
             />
 
             <Route path="/host" element={<HostHome />} />
-            <Route path="/host/addHost" element={<AddHost />} />
-            <Route path="/host/addSpace" element={<AddSpace />} />
+
+            {/* <Route path="/host/addHost" element={<AddHost />} /> */}
+            {/* <Route path="/host/addSpace" element={<AddSpace />} /> */}
             <Route path="/host/bookList" element={<HostBookList />} />
             <Route path="/host/spaceList" element={<HostSpaceList />} />
-            <Route path="/host/updateSpace" element={<UpdateSpace />} />
+            {/* <Route path="/host/updateSpace" element={<UpdateSpace />} /> */}
 
-            <Route path="/list/:searchInput" element={<ProductList />} />
+            {/* <Route path="/list/:searchInput" element={<ProductList />} />
             <Route path="/detail/:spaceId" element={<Detail />} />
             <Route path="/book" element={<Book />} />
 
             <Route path="/" element={<Home />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes> */}
+
+            <Routes path="*" element={<NotFound />} /> */}
+          </Routes>
           <Footer></Footer>
         </BrowserRouter>
       </div>
--- client/src/components/Chart.jsx
+++ client/src/components/Chart.jsx
@@ -6,62 +6,51 @@ import {
   YAxis,
   Tooltip,
   Legend,
+  LabelList,
   Label,
   Bar,
 } from "recharts";
 import styled from "styled-components";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+async function getData() {
+  const url = "/chartMockData.json";
+  const mockdata = await axios.get(url);
+
+  console.log(mockdata.data);
+  return mockdata;
+}
 
 function Chart() {
-  const data = [
-    {
-      name: "Page A",
-      uv: 4000,
-      pv: 2400,
-    },
-    {
-      name: "Page B",
-      uv: 3000,
-      pv: 1398,
-    },
-    {
-      name: "Page C",
-      uv: 2000,
-      pv: 9800,
-    },
-    {
-      name: "Page D",
-      uv: 2780,
-      pv: 3908,
-    },
-    {
-      name: "Page E",
-      uv: 1890,
-      pv: 4800,
-    },
-    {
-      name: "Page F",
-      uv: 2390,
-      pv: 3800,
-    },
-    {
-      name: "Page G",
-      uv: 3490,
-      pv: 4300,
-    },
-  ];
+  const [chartData, setChartData] = useState([]);
+  const url = "/chartMockData.json";
+
+  useEffect(() => {
+    async function getData() {
+      const response = await axios.get(url);
+      const data = response.data;
+      setChartData(data);
+    }
+    getData();
+  }, []);
 
   return (
     <Box>
       <Title>일별 매출 수</Title>
       <ResponsiveContainer width={"100%"} height={"100%"}>
-        <BarChart data={data}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name"></XAxis>
-          <YAxis></YAxis>
+        <BarChart data={chartData} barSize={(35, 35)} barGap={10}>
+          <CartesianGrid strokeDasharray="5 5" />
+          <XAxis dataKey="roomName"></XAxis>
+          <YAxis type="number" domain={[0, "dataMax+50"]}></YAxis>
           <Tooltip />
           <Legend type="diamond" />
-          <Bar dataKey="pv" fill="#8884d8" />
-          <Bar dataKey="uv" fill="#82ca9d" />
+          <Bar dataKey="beforeCount" fill="#8884d8">
+            <LabelList dataKey="beforeCount" position="top" />
+          </Bar>
+          <Bar dataKey="count" fill="#82ca9d">
+            <LabelList dataKey="count" position="top" />
+          </Bar>
         </BarChart>
       </ResponsiveContainer>
     </Box>
