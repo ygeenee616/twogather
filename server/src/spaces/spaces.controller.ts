2fc970e2f91223b2dba7aea21a5dcf200dfa211b
[SW2/서울/2반] 김태훈 <kth5954@gmail.com>
2022-07-15 04:45:42 +0900

    feature: 공간 API(등록, 조회 컨트롤러 및 서비스 개발, entity 관계설정)

--- server/src/spaces/spaces.controller.ts
+++ server/src/spaces/spaces.controller.ts
@@ -6,18 +6,22 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { SpacesService } from './spaces.service';
 import { CreateSpaceDto } from './dto/create-space.dto';
 import { UpdateSpaceDto } from './dto/update-space.dto';
+import { GetUser } from 'src/custom.decorator';
+import { AuthGuard } from '@nestjs/passport';
 
-@Controller('spaces')
+@Controller('api/spaces')
 export class SpacesController {
   constructor(private readonly spacesService: SpacesService) {}
 
   @Post()
-  create(@Body() createSpaceDto: CreateSpaceDto) {
-    return this.spacesService.create(createSpaceDto);
+  @UseGuards(AuthGuard())
+  async create(@GetUser() user, @Body() createSpaceDto: CreateSpaceDto) {
+    return this.spacesService.create(createSpaceDto, user);
   }
 
   @Get()
--- server/src/spaces/spaces.service.ts
+++ server/src/spaces/spaces.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { ConsoleLogger, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/users.entity';
 import { Repository } from 'typeorm';
 import { CreateSpaceDto } from './dto/create-space.dto';
 import { Space } from './entities/spaces.entity';
@@ -11,25 +12,56 @@ export class SpacesService {
     private spacesRepository: Repository<Space>,
   ) {}
 
-  async create(spaceData: CreateSpaceDto): Promise<Space> {
+  async create(spaceData: CreateSpaceDto, user: User): Promise<Space> {
     try {
-      return await this.spacesRepository.save(spaceData);
+      const newSpace = {
+        ...spaceData,
+        user,
+      };
+      return await this.spacesRepository.save(newSpace);
     } catch (error) {
       throw error;
     }
   }
-  a;
 
   async findAll(): Promise<Space[]> {
-    return this.spacesRepository.find();
-  }
-
-  async findOne(id: number): Promise<Space> {
-    return this.spacesRepository.findOneBy({
-      id,
+    return await this.spacesRepository.find({
+      relations: {
+        user: true,
+      },
+      order: {
+        id: 'ASC',
+      },
+      cache: true,
     });
   }
 
+  // id로 공간 조회
+  async findOne(id: number): Promise<Space> {
+    return this.spacesRepository.findOne({
+      where: {
+        id,
+      },
+      relations: {
+        user: true,
+      },
+      cache: true,
+    });
+  }
+
+  async findByType(type: string): Promise<Space[]> {
+    return this.spacesRepository.find({
+      where: {
+        type,
+      },
+      relations: {
+        user: true,
+      },
+      cache: true,
+    });
+  }
+
+  // 공간 삭제
   async remove(id: number): Promise<void> {
     await this.spacesRepository.delete(id);
   }
