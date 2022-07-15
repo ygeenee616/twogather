fb62238704069b0ec46598f852831d39809157bb
[SW2/서울/2반] 김태훈 <kth5954@gmail.com>
2022-07-16 02:43:03 +0900

    chore: 간단한 수정사항

--- server/src/spaces/spaces.service.ts
+++ server/src/spaces/spaces.service.ts
@@ -1,7 +1,4 @@
-import {
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/users.entity';
 import { Repository } from 'typeorm';
