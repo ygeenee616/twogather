0dd88c56178a43b40cb12a5b44bac2a1f8e44bcd
나해란 <haeran@nahaelan-ui-MacBookAir.local>
2022-07-15 15:41:54 +0900

    feat: queryString으로 date, startTime, endTime 받아옴

--- client/src/components/list/ListDatePicker.jsx
+++ client/src/components/list/ListDatePicker.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
 import styled from "styled-components";
 import DatePicker from "react-datepicker";
 import { ko } from "date-fns/esm/locale";
@@ -7,81 +8,105 @@ import "./../../assets/styles/DatePicker.css";
 
 export default function ListDatePicker() {
   const [date, setDate] = useState(new Date());
-  const [startTime, setStartTime] = useState(0);
-  const [endTime, setEndTime] = useState(0);
+  const [startTime, setStartTime] = useState(false);
+  const [endTime, setEndTime] = useState(false);
   const [caution, setCaution] = useState(false);
+
+  const { searchInput } = useParams();
 
   const timeTable = [];
   for (let i = 0; i < 24; i++) {
     timeTable.push(i);
   }
 
+  const handleChangeDatePicker = (date) => {
+    // const yyyy = `${date.getFullYear()}`;
+    // const mm = `0${date.getMonth() + 1}`;
+    // const dd = `${date.getDate()}`;
+    // const fullDate = yyyy + mm + dd;
+    // console.log(fullDate);
+    setDate(date);
+  };
+
   return (
-    <Container caution={caution}>
-      <DatePicker
-        locale={ko}
-        selected={date}
-        minDate={new Date()} // 이전 날짜는 선택 불가
-        inline
-      />
+    <Container>
+      <DatePickerContainer caution={caution}>
+        <DatePicker
+          locale={ko}
+          selected={date}
+          minDate={new Date()} // 이전 날짜는 선택 불가
+          inline
+          onChange={handleChangeDatePicker}
+        />
 
-      <div className="timePicker">
-        <TimeSelect
-          name="timeTable"
-          size="3"
-          onClick={(e) => {
-            setStartTime(Number(e.target.value));
-            Number(e.target.value) < endTime
-              ? setCaution(false)
-              : setCaution(true);
-          }}
-        >
-          <option className="title" disabled>
-            시작 시간
-          </option>
-          {timeTable.map((time, i) => {
-            return (
-              <option key={i} name={time} value={time} className="startTime">
-                {time}:00
-              </option>
-            );
-          })}
-        </TimeSelect>
+        <div className="timePicker">
+          <TimeSelect
+            name="timeTable"
+            size="3"
+            onClick={(e) => {
+              setStartTime(Number(e.target.value));
+              Number(e.target.value) < endTime
+                ? setCaution(false)
+                : setCaution(true);
+            }}
+          >
+            <option className="title" disabled>
+              시작 시간
+            </option>
+            {timeTable.map((time, i) => {
+              return (
+                <option key={i} name={time} value={time} className="startTime">
+                  {time}:00
+                </option>
+              );
+            })}
+          </TimeSelect>
 
-        <span className="bookInfo"> ~ </span>
+          <span className="bookInfo"> ~ </span>
 
-        <TimeSelect
-          name="timeTable"
-          size="3"
-          onClick={(e) => {
-            setEndTime(Number(e.target.value));
-            startTime < Number(e.target.value)
-              ? setCaution(false)
-              : setCaution(true);
-          }}
-        >
-          <option className="title" disabled>
-            종료 시간
-          </option>
-          {timeTable.map((time, i) => {
-            return (
-              <option key={i} name={time} value={time} className="endTime">
-                {time}:00
-              </option>
-            );
-          })}
-        </TimeSelect>
-      </div>
-      <Guide caution={caution}>
-        <p className="caution">*최소 예약시간은 1시간입니다.</p>
-      </Guide>
+          <TimeSelect
+            name="timeTable"
+            size="3"
+            onClick={(e) => {
+              setEndTime(Number(e.target.value));
+              startTime < Number(e.target.value)
+                ? setCaution(false)
+                : setCaution(true);
+            }}
+          >
+            <option className="title" disabled>
+              종료 시간
+            </option>
+            {timeTable.map((time, i) => {
+              return (
+                <option key={i} name={time} value={time} className="endTime">
+                  {time}:00
+                </option>
+              );
+            })}
+          </TimeSelect>
+        </div>
+        <Guide caution={caution}>
+          <p className="caution">*최소 예약시간은 1시간입니다.</p>
+        </Guide>
+      </DatePickerContainer>
+      <ApplyDateButton
+        to={`/list/${searchInput}?date=${date}&startTime=${startTime}&endTime=${endTime}`}
+      >
+        날짜 적용하기
+      </ApplyDateButton>
     </Container>
   );
 }
 
 const Container = styled.div`
-  ${({ caution }) =>
-    caution ? `margin: auto auto 0 auto;` : `margin: auto auto 12.8% auto;`};
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`;
+
+const DatePickerContainer = styled.div`
+  margin: auto;
 `;
 
 const TimeSelect = styled.select`
@@ -121,6 +146,18 @@ const Guide = styled.div`
   color: red;
 
   & .caution {
-    ${({ caution }) => (caution ? `display: block;` : `display: none;`)};
+    ${({ caution }) => (caution ? `display: block;` : `visibility: hidden;`)};
   }
+`;
+
+const ApplyDateButton = styled(Link)`
+  all: unset;
+  width: 100%;
+  height: 10%;
+  padding: 6% 0;
+  color: white;
+  text-align: center;
+  font-weight: 600;
+  background-color: #8daef2;
+  cursor: pointer;
 `;
