398e910ffc3aee1ca91441167afd6cd5b8e5578c
Yeonjin <ygeenee@naver.com>
2022-07-16 03:21:06 +0900

    feat: 마이페이지- 닉네임 수정

--- client/src/components/mypage/MyProfile.jsx
+++ client/src/components/mypage/MyProfile.jsx
@@ -4,9 +4,19 @@ import { useState } from "react"
 
 
 function MyProfile() {
-  const user = useSelector((state) => state.user);
+  const user = {
+    nickname: "연두부",
+    email: "dubu@kakao.com",
+    password: "******",
+    socialLogin: "카카오"
+  }
   const [editNickname, setEditNickname] = useState(true);
+  const [nickname, setNickname] = useState(user.nickname)
 
+  const handleEditNickname = (e) => {
+    e.preventDefault();
+    setEditNickname(!editNickname);
+  }
   // 로그인이 안되어 있을 경우
   return (
 
@@ -16,18 +26,27 @@ function MyProfile() {
         <ProfileImgEditBtn>프로필 수정 </ProfileImgEditBtn>
       </ProfileImgDiv>
       <ProfileInfo>
-        <tr> <Nickname colSpan='2'> {user.nickname} <a href="#" onClick={()=> {setEditNickname(!editNickname)}}>수정</a> </Nickname>  </tr>
+        <tr> 
+          <Nickname colSpan='2'> 
+          {editNickname 
+            ? nickname 
+            : <input value={nickname} onChange={(e)=>setNickname(e.target.value)} placeholder="닉네임을 입력하세요"/>} 
+           
+            { editNickname ? <span className="edit" onClick={handleEditNickname}> 수정 </span>: <button className="doneEditBtn">수정 완료</button> }
+          </Nickname>  
+        </tr>
         <tr>
+
           <InfoTag>이메일</InfoTag>
           <InfoTD> {user.email} </InfoTD>
         </tr>
         <tr>
           <InfoTag>비밀번호</InfoTag>
-          <InfoTD><a href="/">비밀번호 변경</a></InfoTD>
+          <InfoTD><span>비밀번호 변경</span></InfoTD>
         </tr>
         <tr>
           <InfoTag>소셜 로그인 연동</InfoTag>
-          <InfoTD>카카오</InfoTD>
+          <InfoTD>{user.socialLogin}</InfoTD>
         </tr>
       </ProfileInfo>
     </ProfileDiv>
@@ -40,7 +59,7 @@ function MyProfile() {
 const ProfileDiv = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: center;
+  justify-content: left;
   height: wrap-content;
   border: none;
   text-align: center;
@@ -70,7 +89,7 @@ const ProfileImgEditBtn = styled.div`
 const ProfileInfo = styled.table`
   border-collapse: collapse;
   border-spacing: 0;
-
+  margin-left: 3rem;
 
   tr {
     width: 100%;
@@ -82,7 +101,31 @@ const Nickname = styled.td`
   font-size: 2rem;
   font-weight: bold;
   text-align: left;
+  .edit {
+    margin-left: 1rem;
+    font-size: 0.8rem;
+    color: grey;
+    text-decoration: underline;
+    cursor: pointer;
+  }
+  input {
+    height: 2rem;
+    padding: 0 0.5rem;
+  }
 
+  .doneEditBtn {
+    background-color: light-grey;
+    width: 5rem;
+    height: 2.5rem;
+    padding: 0.5rem;
+    margin: 0 1rem;
+    border: solid;
+    font-weight: bold;
+
+    :hover {
+      box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.25);
+    }
+  }
 `
 
 const InfoTag = styled.td`
