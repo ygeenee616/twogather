23fc52cb93b65a17e02c61c00a0c0f74d61999b3
[SW2/서울/2반] 김태훈 <kth5954@gmail.com>
2022-07-17 17:45:14 +0900

    feature: 예약 API 수정

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get, Header, Param, Response } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('api')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    const apiDocs = 'http://localhost:3000/api-docs';
+    return `
+    <div><H1>Twogather Backend API</H1></div>
+    Welcome to visit the Twogather Backend API Home.<br>
+    Please click <a href=${apiDocs}>here</a> want to see the api-docs.
+    
+    `;
   }
 }
