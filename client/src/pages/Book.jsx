8758218dda37e1483e3e97c63f1430f362c54444
miji <mijiunknown@gmail.com>
2022-07-18 00:32:27 +0900

    feat: detail, book api 뼈대만

--- client/src/pages/Book.jsx
+++ client/src/pages/Book.jsx
@@ -7,6 +7,7 @@ import BookInfo from "../components/book/BookInfo";
 import ToTop from "../components/ToTop";
 import axios from "axios";
 import { useEffect } from "react";
+import * as Api from "../api";
 
 export default function Book() {
   const location = useLocation();
@@ -61,6 +62,21 @@ export default function Book() {
       : (possible.current = false);
   }
 
+  // 예약 등록 함수
+  async function submitBook(id) {
+    try {
+      // const req = await Api.post(`/api/reservations/3`, {
+      //   startTime: 18,
+      //   endTime: 20,
+      //   date: "2022-07-20",
+      //   personnel: 3,
+      // });
+      console.log("예약완료");
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <FullContainer>
       <GetBookerInfo
@@ -87,7 +103,7 @@ export default function Book() {
       />
       <BookBox possible={possible.current}>
         <p className="required">*예약 필수 정보를 입력해주세요</p>
-        <button onClick={() => possible.current && console.log("예약완료")}>
+        <button onClick={() => possible.current && submitBook()}>
           예약 완료
         </button>
       </BookBox>
