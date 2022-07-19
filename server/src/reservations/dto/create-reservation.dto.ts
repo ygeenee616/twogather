11b48594c69bbe5b461914a4bb9c79c236187580
[SW2/서울/2반] 김태훈 <kth5954@gmail.com>
2022-07-20 04:07:47 +0900

    fix: reservation entity 및 dto 수정(예약자, 이메일, 휴대폰 번호 추가)

--- server/src/reservations/dto/create-reservation.dto.ts
+++ server/src/reservations/dto/create-reservation.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsDateString, IsNumber, IsString } from 'class-validator';
+import { IsDateString, IsEmail, IsNumber, IsString } from 'class-validator';
 export class CreateReservationDto {
   @IsNumber()
   @ApiProperty({ description: '시작 시간' })
@@ -16,6 +16,18 @@ export class CreateReservationDto {
   @IsNumber()
   @ApiProperty({ description: '예약 인원' })
   personnel: number;
+
+  @IsString()
+  @ApiProperty({ description: '예약 유저 이름' })
+  reserveUsername: string;
+
+  @IsString()
+  @ApiProperty({ description: '예약 휴대폰 번호' })
+  reservePhoneNumber: string;
+
+  @IsEmail()
+  @ApiProperty({ description: '예약 이메일' })
+  reserveEmail: string;
 
   @IsString()
   @ApiProperty({ description: '사용 목적' })
