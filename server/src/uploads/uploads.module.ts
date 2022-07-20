b03aa8dd83c8d8bd806f9b7215214257c1c34862
JongwonJang <phobos90@naver.com>
2022-07-21 02:18:34 +0900

    feat: 이미지 업로드 api 구현, imgaeUrl entity 수정

--- server/src/uploads/uploads.module.ts
+++ server/src/uploads/uploads.module.ts
@@ -6,15 +6,33 @@ import { SpaceImagesModule } from 'src/space_images/space_images.module';
 import { SpaceImagesService } from 'src/space_images/space_images.service';
 import { SpacesService } from 'src/spaces/spaces.service';
 import { SpacesModule } from 'src/spaces/spaces.module';
+import { UsersModule } from 'src/users/users.module';
+import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { RoomImagesModule } from 'src/room_images/room_images.module';
+import { RoomImagesService } from 'src/room_images/room_images.service';
+import { RoomsService } from 'src/rooms/rooms.service';
+import { RoomsModule } from 'src/rooms/rooms.module';
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
     SpaceImagesModule,
+    RoomImagesModule,
+    UsersModule,
     SpacesModule,
+    RoomsModule,
   ],
   controllers: [UploadsController],
-  providers: [UploadsService, SpaceImagesService, SpacesService],
+  providers: [
+    UploadsService,
+    SpaceImagesService,
+    RoomImagesService,
+    UsersService,
+    JwtService,
+    SpacesService,
+    RoomsService,
+  ],
 })
 export class UploadsModule {
   constructor(private uploadsService: UploadsService) {}
