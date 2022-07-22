92c73c8f0bcb2d04b5bc58619d288fce1e627408
Yeonjin <ygeenee@naver.com>
2022-07-22 12:23:01 +0900

    fix: 로그인만료, notfound, 가입완료 문구

--- client/src/pages/LoginExpired.jsx
+++ client/src/pages/LoginExpired.jsx
@@ -3,7 +3,12 @@ import MessagePage from "../components/MessagePage";
 function LoginExpired() {
   localStorage.clear();
 
-  return <MessagePage content="로그인이 만료되었습니다."></MessagePage>;
+  return (
+    <MessagePage
+      title="로그인이 필요합니다."
+      content="로그인이 만료되었거나, 권한이 없습니다."
+    ></MessagePage>
+  );
 }
 
 export default LoginExpired;
