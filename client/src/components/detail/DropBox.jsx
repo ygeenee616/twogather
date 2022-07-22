00405db835320c96d340d698a9a8170545717f4e
JongwonJang <phobos90@naver.com>
2022-07-22 14:18:39 +0900

    fix: roomImage 오류 수정

--- client/src/components/detail/DropBox.jsx
+++ client/src/components/detail/DropBox.jsx
@@ -5,18 +5,24 @@ import { FcHome, FcConferenceCall } from "react-icons/fc";
 import * as Api from "../../api";
 
 export default function Dropbox({ rooms, acceptPeople, checkSelectRoom }) {
-  const [image, setImage] = useState("");
-  const imageUrl = useRef("");
+  const [roomUrl, setRoomUrl] = useState([]);
 
   useEffect(() => {
-    setImage(imageUrl.current);
-  }, [imageUrl.current]);
+    const take = async () => {
+      for (let i = 0; i < rooms.length; i++) {
+        const imageUrl = await Api.getAuth(
+          `api/room-images/room/${rooms[i].id}`
+        );
+        const i2 = imageUrl.data.data[0].imageUrl;
+        setRoomUrl((prev) => [...prev, i2]);
+      }
+    };
+    take();
+  }, []);
 
   const getImg = async (id) => {
     try {
       const roomImageReq = await Api.getAuth(`api/room-images/room/${id}`);
-      imageUrl.current = roomImageReq.data.data[0].imageUrl;
-      return imageUrl.current;
     } catch (err) {
       console.log(err);
     }
@@ -26,50 +32,44 @@ export default function Dropbox({ rooms, acceptPeople, checkSelectRoom }) {
     rooms && (
       <RoomList>
         <p>세부 공간 선택</p>
-
         {rooms.map((room, i) => {
           // api 데이터 받아오는 함수
 
-          getImg(room.id);
-          setImage(imageUrl.current);
+          // getImg(room.id);
+          // setImage(imageUrl.current);
           return (
-            image !== "" && (
-              <Container key={i}>
-                <RoomItem>
-                  <input
-                    type="radio"
-                    id="select"
-                    name="room"
-                    className={room.name}
-                    value={room.id}
-                    onClick={(e) => {
-                      checkSelectRoom(room.id, room.name, room.price);
-                      acceptPeople.current = room.capacity;
-                      console.log(acceptPeople.current);
-                    }}
-                  />
-                  <RoomLabel>
-                    <span>{room.name}</span>
-                    <span>시간당 {room.price} ₩</span>
-                  </RoomLabel>
-                  <img src={image} />
-                  <HiChevronDown />
-                </RoomItem>
-                <Dropdown>
-                  <div>
-                    <FcHome style={{ marginRight: "5px" }} size={16} />
-                    공간 유형 : {room.description}
-                  </div>
-                  <div>
-                    <FcConferenceCall
-                      style={{ marginRight: "5px" }}
-                      size={16}
-                    />
-                    수용 인원 : {room.capacity}
-                  </div>
-                </Dropdown>
-              </Container>
-            )
+            <Container key={i}>
+              <RoomItem>
+                <input
+                  type="radio"
+                  id="select"
+                  name="room"
+                  className={room.name}
+                  value={room.id}
+                  onClick={(e) => {
+                    checkSelectRoom(room.id, room.name, room.price);
+                    acceptPeople.current = room.capacity;
+                    console.log(acceptPeople.current);
+                  }}
+                />
+                <RoomLabel>
+                  <span>{room.name}</span>
+                  <span>시간당 {room.price} ₩</span>
+                </RoomLabel>
+                <img src={roomUrl[i]} />
+                <HiChevronDown />
+              </RoomItem>
+              <Dropdown>
+                <div>
+                  <FcHome style={{ marginRight: "5px" }} size={16} />
+                  공간 유형 : {room.description}
+                </div>
+                <div>
+                  <FcConferenceCall style={{ marginRight: "5px" }} size={16} />
+                  수용 인원 : {room.capacity}
+                </div>
+              </Dropdown>
+            </Container>
           );
         })}
       </RoomList>
